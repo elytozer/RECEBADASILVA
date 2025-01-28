1786437779fb0dee543488e16d74c46c57211635
elyton <elyton.sousa@academico.ifpb.edu.br>
2025-01-28 17:44:55 +0000

    28/01/25

---
+++ js/heroScript.js
@@ -0,0 +1,7 @@
+// Escutando o clique no botão
+document.getElementById("botaoAtk").addEventListener("click", function () {
+    let nomeHeroi = document.getElementById("nomePlayer").value; // Obtém o nome
+    console.log("Ataque realizado!");
+    console.log(`Bem-vindo, ${nomePlayer}!`);
+  });
+  
