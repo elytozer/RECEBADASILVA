0ab221a81dd293180ae3fc3c29034865cae43f7a
elyton <elyton.sousa@academico.ifpb.edu.br>
2025-02-18 16:45:48 -0300

    18/02/2025

--- jsExterno/js/heroScript.js
+++ jsExterno/js/heroScript.js
@@ -1,7 +1,16 @@
 let nomePlayer = "altria";// armazena o nome
 let playerCoin = 5;
 let playerVd = 3;
-let playerLev = 1;
+let nivel = 15; // Altere o nível do jogador conforme necessário
+  if (nivel >= 1 && nivel <= 10) {
+    console.log("Fácil");
+  } else if (nivel >= 11 && nivel <= 20) {
+    console.log("Normal");
+  } else if (nivel >= 21) {
+    console.log("Difícil");
+  } else {
+    console.log("Nível inválido");
+  }
 let playerAtk = 20;
 let playerDef = 15;
 let dano = playerAtk - playerDef ; 
@@ -9,7 +18,26 @@ let playerHp = prompt('informe o HP do personagem:')
   if (playerHp > 0) {
       console.log('o personagem esta vivo ')
   } else console.log('presunto');  
-let wrdChalenge = while(playerLev <= 10)
+let contador = 5;
+  while (contador >= 0) {
+      if (contador > 0) {
+          console.log(contador);
+      } else {
+          console.log("Batalha iniciada!");
+      }
+      contador--;
+  }
+let inimigos = ["Goblin", "Orc", "Troll"];
+  for (let i = 0; i < inimigos.length; i++) {
+      console.log(inimigos[i]);
+  }
+// Exemplo de uso: calcular o dano em um inimigo com defesa 10
+let ataque = 20;
+let defesaInimigo = 10;
+let danoCausado = calcularDano(ataque, defesaInimigo);
+
+console.log(`Dano causado: ${danoCausado}`);
+
 
 console.log(nomePlayer);
 console.log("o heroi tem, ${playerCoin}");
@@ -22,3 +50,7 @@ document.getElementById("botaoAtk").addEventListener("click", function () {
     console.log(`Bem-vindo, ${nomePlayer}!`);
   });
   
+function calcularDano(ataque, defesa) {
+    let dano = ataque - defesa;
+    return dano > 0 ? dano : 0; // O dano mínimo é 0 (não pode ser negativo)
+}
